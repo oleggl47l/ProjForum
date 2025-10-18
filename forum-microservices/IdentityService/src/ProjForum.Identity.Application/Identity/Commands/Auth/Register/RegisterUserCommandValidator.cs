822b978b71544036c412c23c653cb6ca7cb9689a
olegg <s.gelo2004@gmail.com>
2025-10-18 13:17:40 +0300

    chore: add validator for register

---
+++ forum-microservices/IdentityService/src/ProjForum.Identity.Application/Identity/Commands/Auth/Register/RegisterUserCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace ProjForum.Identity.Application.Identity.Commands.Auth.Register;
+
+public class RegisterUserCommandValidator : AbstractValidator<RegisterUserCommand>
+{
+    public RegisterUserCommandValidator()
+    {
+        RuleFor(x => x.UserName)
+            .NotEmpty().WithMessage("User Name is required.")
+            .MaximumLength(20).WithMessage("User Name must not exceed 20 characters.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty().WithMessage("Email is required.")
+            .Matches(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$").WithMessage("Invalid email format.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty().WithMessage("Password is required.")
+            .MinimumLength(8).WithMessage("Password must be at least 8 characters.")
+            .Matches(@"[A-ZА-Я]").WithMessage("Password must contain at least one uppercase letter.")
+            .Matches(@"[a-zа-я]").WithMessage("Password must contain at least one lowercase letter.")
+            .Matches(@"[0-9]").WithMessage("Password must contain at least one digit.")
+            .Matches(@"[\W_]")
+            .WithMessage("Password must contain at least one special character (e.g., !, @, #, $, etc.).");
+    }
+}
