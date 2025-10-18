441d54a1d510a332af3c9a5b3708dc262b6fb999
olegg <s.gelo2004@gmail.com>
2025-10-18 12:43:00 +0300

    feat: add api extensions, update program.cs, add swagger cnfg

--- forum-microservices/IdentityService/src/ProjForum.Identity.Api/Program.cs
+++ forum-microservices/IdentityService/src/ProjForum.Identity.Api/Program.cs
@@ -1,9 +1,5 @@
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
-using ProjForum.Identity.Api.ExceptionHandlers;
+using ProjForum.Identity.Api.Extensions;
 using ProjForum.Identity.Application.Extensions;
 using ProjForum.Identity.Infrastructure.Persistence;
 using ProjForum.Identity.Infrastructure.Persistence.Extensions;
@@ -21,79 +17,11 @@ try
     var configuration = builder.Configuration;
 
     builder.Services.AddInfrastructure(builder.Configuration);
-
-    builder.Services.AddControllers();
-
-    Console.WriteLine("\n\n\nDatabase Connection String: " + configuration.GetConnectionString("PFIdentity") +
-                      "\n\n\n");
-
     builder.Services.AddApplication();
-    builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
-    builder.Services.AddProblemDetails();
-
-    builder.Services.AddEndpointsApiExplorer();
-
-    builder.Services.AddSwaggerGen(opt =>
-    {
-        opt.SwaggerDoc("v1", new OpenApiInfo { Title = "ProjForumIdentityAPI", Version = "v1" });
-        opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Description = "Enter JWT token",
-            Name = "Authorization",
-            Type = SecuritySchemeType.Http,
-            BearerFormat = "JWT",
-            Scheme = "bearer"
-        });
-        opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                []
-            }
-        });
-    });
-
-    builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-            }
-        )
-        .AddJwtBearer(options =>
-            {
-                options.SaveToken = true;
-                options.RequireHttpsMetadata = false;
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidateAudience = true,
-                    ValidAudience = builder.Configuration["JwtSettings:Audience"],
-                    ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
-                    ClockSkew = TimeSpan.Zero,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(
-                        builder.Configuration["JwtSettings:SecretKey"] ?? throw new InvalidOperationException()))
-                };
-            }
-        );
-
-    builder.Services.AddAuthorization(options =>
-    {
-        options.AddPolicy("Admin", policy => policy.RequireRole("Admin"));
-        options.AddPolicy("User", policy => policy.RequireRole("User"));
-        options.AddPolicy("Editor", policy => policy.RequireRole("Editor"));
-    });
+    builder.Services.AddApiServices(configuration);
+    builder.Services.AddSwagger(configuration);
 
     var app = builder.Build();
-
 
     using (var scope = app.Services.CreateScope())
     {
@@ -104,18 +32,12 @@ try
     await app.UseIdentitySeedingAsync();
 
     if (app.Environment.IsDevelopment())
-    {
-        app.UseSwagger();
-        app.UseSwaggerUI();
-    }
+        app.UseSwaggerUi(configuration);
 
     app.UseHttpsRedirection();
-
     app.UseAuthentication();
     app.UseAuthorization();
-
     app.MapControllers();
-
     app.UseExceptionHandler();
 
     app.Run();
