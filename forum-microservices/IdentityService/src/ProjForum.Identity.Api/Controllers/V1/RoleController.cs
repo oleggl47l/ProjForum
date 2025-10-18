94c2cda275e93097cf93772157cf9522e12dccf3
olegg <s.gelo2004@gmail.com>
2025-10-18 14:35:46 +0300

    feat: add role endpoints

--- forum-microservices/IdentityService/src/ProjForum.Identity.Api/Controllers/V1/RoleController.cs
+++ forum-microservices/IdentityService/src/ProjForum.Identity.Api/Controllers/V1/RoleController.cs
@@ -1,51 +1,82 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using ProjForum.Identity.Application.DTOs;
+using ProjForum.Identity.Application.DTOs.Role;
 using ProjForum.Identity.Application.Identity.Commands.Roles.CreateRole;
 using ProjForum.Identity.Application.Identity.Commands.Roles.DeleteRole;
 using ProjForum.Identity.Application.Identity.Commands.Roles.UpdateRole;
 using ProjForum.Identity.Application.Identity.Queries.Roles.GetAllRoles;
 using ProjForum.Identity.Application.Identity.Queries.Roles.GetRole;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace ProjForum.Identity.Api.Controllers.V1;
 
-[Route("api/v1/[controller]/[action]")]
 [ApiController]
-
-//TODO:  update controller
-public class RoleController(IMediator mediator) : ControllerBase
+[Route("api/[controller]")]
+[Authorize(Policy = "Admin")]
+public class RolesController(IMediator mediator) : ControllerBase
 {
-    // [HttpGet("{id}")]
-    // public async Task<ActionResult> GetById(string id)
-    // {
-    //     var role = await mediator.Send(new GetRoleQuery { RoleId = id });
-    //     return Ok(role);
-    // }
-    //
-    // [HttpGet]
-    // public async Task<ActionResult> GetAll()
-    // {
-    //     var roles = await mediator.Send(new GetAllRolesQuery());
-    //     return Ok(roles);
-    // }
-    //
-    // [HttpPost]
-    // public async Task<IActionResult> Create([FromBody] CreateRoleCommand command)
-    // {
-    //     var result = await mediator.Send(command);
-    //     return Ok(result);
-    // }
-    //
-    // [HttpPatch]
-    // public async Task<IActionResult> Update([FromBody] UpdateRoleCommand command)
-    // {
-    //     var result = await mediator.Send(command);
-    //     return Ok(result);
-    // }
-    //
-    // [HttpDelete("{id}")]
-    // public async Task<IActionResult> Delete(string id)
-    // {
-    //     await mediator.Send(new DeleteRoleCommand { RoleId = id });
-    //     return Ok();
-    // }
+    /// <summary>
+    /// Получить список всех ролей
+    /// </summary>
+    [HttpGet]
+    [AllowAnonymous]
+    [SwaggerOperation(OperationId = "GetAllRoles")]
+    public async Task<ActionResult<IReadOnlyList<RoleDto>>> GetAll(CancellationToken cancellationToken)
+    {
+        var roles = await mediator.Send(new GetAllRolesQuery(), cancellationToken);
+        return Ok(roles);
+    }
+
+    /// <summary>
+    /// Получить роль по Id
+    /// </summary>
+    [HttpGet("{id:guid}")]
+    [AllowAnonymous]
+    [SwaggerOperation(OperationId = "GetRoleById")]
+    public async Task<ActionResult<RoleDto>> GetById(Guid id, CancellationToken cancellationToken)
+    {
+        var role = await mediator.Send(new GetRoleByIdQuery(id), cancellationToken);
+        if (role is null)
+            return NotFound(new OperationResultDto(false, "Role not found"));
+
+        return Ok(role);
+    }
+
+    /// <summary>
+    /// Создать новую роль
+    /// </summary>
+    [HttpPost]
+    [SwaggerOperation(OperationId = "CreateRole")]
+    public async Task<ActionResult<CreateRoleResultDto>> Create([FromBody] CreateRoleCommand command,
+        CancellationToken cancellationToken)
+    {
+        var result = await mediator.Send(command, cancellationToken);
+        return CreatedAtAction(nameof(GetById), new { id = result.Role.Id }, result);
+    }
+    
+    /// <summary>
+    /// Обновить роль
+    /// </summary>
+    [HttpPut("{id:guid}")]
+    [SwaggerOperation(OperationId = "UpdateRole")]
+    public async Task<ActionResult<UpdateRoleResultDto>> Update(Guid id, [FromBody] UpdateRoleCommand command,
+        CancellationToken cancellationToken)
+    {
+        command = command with { Id = id };
+        var result = await mediator.Send(command, cancellationToken);
+        return Ok(result);
+    }
+
+    /// <summary>
+    /// Удалить роль
+    /// </summary>
+    [HttpDelete("{id:guid}")]
+    [SwaggerOperation(OperationId = "DeleteRole")]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        await mediator.Send(new DeleteRoleCommand(id), cancellationToken);
+        return NoContent();
+    }
 }
