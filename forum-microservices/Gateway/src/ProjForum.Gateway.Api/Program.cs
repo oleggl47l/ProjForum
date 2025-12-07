1e5d40e26102611149638656bc2599f6e02de6ac
olegg <s.gelo2004@gmail.com>
2025-12-07 19:05:16 +0300

    [gateway] add serilog

---
+++ forum-microservices/Gateway/src/ProjForum.Gateway.Api/Program.cs
@@ -0,0 +1,38 @@
+using Serilog;
+
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .ReadFrom.Configuration(new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build()).WriteTo.Console()
+    .CreateLogger();
+try
+{
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
+
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    var app = builder.Build();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseRouting();
+    app.MapControllers();
+    app.UseAuthentication();
+
+    app.Run();
+}
+catch (Exception e)
+{
+    Log.Fatal(e, "Application terminated unexpectedly");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
